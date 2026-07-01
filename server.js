b96be594f9088ed4bbbf3e95fecf68f1e2372527
aswinsanth <aswinsanth123@gmail.com>
2026-07-01 19:33:58 +0530

    added new new  comments

--- server.js
+++ server.js
@@ -5,3 +5,6 @@ const greet = () => {
 // git init               -- to intialize git
 // git add .              -- to add all files
 //git add filename        -- to add certain files
+// git status             -- status of the file tree
+// git log                -- log of the commited files
+// git checkout commit id --to back to previous state
